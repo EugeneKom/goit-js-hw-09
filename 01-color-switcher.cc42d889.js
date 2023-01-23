d1c5d9a1b043c8afd99435c49c993b2072270b81
EugeneKom <100124837+EugeneKom@users.noreply.github.com>
2023-01-23 15:39:10 +0000

    Deploying to gh-pages from @ EugeneKom/goit-js-hw-09@5d31468b5551efb42f35f76f3a82947eb6b307a0 🚀

---
+++ 01-color-switcher.cc42d889.js
@@ -0,0 +1,2 @@
+!function(){var t={btnStart:document.querySelector("button[data-start]"),btnStop:document.querySelector("button[data-stop]"),bodyEl:document.querySelector("body")},n=null;function e(){t.bodyEl.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}t.btnStart.addEventListener("click",(function(){n=setInterval(e,1e3),t.btnStart.disabled=!0,t.btnStop.disabled=!1})),t.btnStop.addEventListener("click",(function(){clearInterval(n),t.btnStart.disabled=!1,t.btnStop.disabled=!0}))}();
+//# sourceMappingURL=01-color-switcher.cc42d889.js.map
